73eb32c00caed80383eda1e0ee4b0957d4537705
Asher <asher_3@hotmail.co.uk>
2020-02-16 18:21:37 +0000

    adding commas and decimal points

--- script.js
+++ script.js
@@ -2,6 +2,72 @@ class Calculator {
 	constructor(previousOperandTextElement, currentOperandTextElement) {
 		this.previousOperandTextElement = previousOperandTextElement;
 		this.currentOperandTextElement = currentOperandTextElement;
+		this.clear();
+	}
+
+	clear() {
+		this.currentOperand = '';
+		this.previousOperand = '';
+		this.operation = undefined;
+	}
+
+	delete() {
+		this.currentOperand = this.currentOperand.toString().slice(0, -1);
+	}
+
+	appendNumber(number) {
+		if (number === '.' && this.currentOperand.includes('.')) return;
+		this.currentOperand = this.currentOperand.toString() + number.toString();
+	}
+
+	chooseOperation(operation) {
+		if (this.currentOperand === '') return;
+		if (this.previousOperand !== '') {
+			this.compute();
+		}
+		this.operation = operation;
+		this.previousOperand = this.currentOperand;
+		this.currentOperand = '';
+	}
+
+	compute() {
+		let computation;
+		const prev = parseFloat(this.previousOperand);
+		const current = parseFloat(this.currentOperand);
+		if (isNan(prev) || isNaN(current)) return;
+		switch (this.operation) {
+			case '+':
+				computation = prev + current;
+				break;
+			case '-':
+				computation = prev - current;
+				break;
+			case '*':
+				computation = prev * current;
+				break;
+			case '÷':
+				computation = prev / current;
+				break;
+			default:
+				return;
+		}
+		this.currentOperand = computation;
+		this.operation = undefined;
+		this.previousOperand = '';
+	}
+
+	getDisplayNumber(number) {
+		const floatNumber = parseFloat(number);
+		if (isNan(floatNumber)) return '';
+		return number.toLocaleString('en');
+	}
+
+	updateDisplay() {
+		this.currentOperandTextElement.innerText = this.getDisplayNumber(this.currentOperand);
+		if (this.operation != null) {
+			this.previousOperandTextElement.innerText = `${this.previousOperand} ${this.operation}`;
+		}
+		this.previousOperandTextElement.innerText = this.previousOperand;
 	}
 }
 
@@ -13,3 +79,34 @@ const allClearButton = document.querySelector('[data-delete]');
 const previousOperandTextElement = document.querySelector('[data-all-clear]');
 const previousOperandTextElement = document.querySelector('[data-previous-operand]');
 const currentOperandTextElement = document.querySelector('[data-current-operand]');
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement);
+
+numberButtons.forEach((button) => {
+	button.addEventListener('click', () => {
+		calculator.appendNumber(button.innerText);
+		calculator.updateDisplay();
+	});
+});
+
+operationButtons.forEach((button) => {
+	button.addEventListener('click', () => {
+		calculator.chooseOperation(button.innerText);
+		calculator.updateDisplay();
+	});
+});
+
+equalsButton.addEventListener('click', (button) => {
+	calculator.compute();
+	calculator.updateDisplay();
+});
+
+allClearButton.addEventListener('click', (button) => {
+	calculator.clear();
+	calculator.updateDisplay();
+});
+
+deleteButton.addEventListener('click', (button) => {
+	calculator.clear();
+	calculator.updateDisplay();
+});
